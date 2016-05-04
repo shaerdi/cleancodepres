28d9c63ad1a9a1016ce88ee0d4b20bd0dd68ad3d
shaerdi <shaerdi@gmail.com>
2016-05-04 11:40:45 +0200

    Added Matlab Unit Test examples

---
+++ MatlabUnitTest/quadraticSolverTest.m
@@ -0,0 +1,23 @@
+function tests = quadraticSolverTest
+    tests = functiontests(localfunctions);
+end
+
+function testRealSolution(testCase)
+    actSolution = quadraticSolver(1,-3,2);
+    expSolution = [2 1];
+    verifyEqual(testCase,actSolution,expSolution)
+end
+
+function testImaginarySolution(testCase)
+    actSolution = quadraticSolver(1,2,10);
+    expSolution = [-1+3i -1-3i];
+    verifyEqual(testCase,actSolution,expSolution)
+end
+
+function testWrongInput(testCase)
+    verifyError(...
+        testCase,...
+        @() quadraticSolver('a',2,1),...
+        'quadraticSolver:InputMustBeNumeric'...
+        );
+end
