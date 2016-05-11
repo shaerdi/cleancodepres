dd82552b7f4840271148a285fedbcbbe0c28447e
shaerdi <shaerdi@gmail.com>
2016-05-11 09:08:36 +0200

    content

--- MatlabUnitTest/quadraticSolverTest.m
+++ MatlabUnitTest/quadraticSolverTest.m
@@ -14,6 +14,12 @@ function testImaginarySolution(testCase)
     verifyEqual(testCase,actSolution,expSolution)
 end
 
+function testVectorInput(testCase)
+    actSolution = quadraticSolver([1;1],[-3;2],[2;10]);
+    expSolution = [2 1; -1+3i -1-3i];
+    verifyEqual(testCase,actSolution,expSolution)
+end
+
 function testWrongInput(testCase)
     verifyError(...
         testCase,...
